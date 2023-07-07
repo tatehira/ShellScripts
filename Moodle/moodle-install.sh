9f08fce86a07f2decd27ff4bbd57f4b368c3847a
Nick Tatehira <68212041+tatehira@users.noreply.github.com>
2023-07-07 13:46:44 -0400

    Update moodle-install.sh

--- Moodle/moodle-install.sh
+++ Moodle/moodle-install.sh
@@ -1,7 +1,7 @@
 !#/bin/bash/
 clear
 echo -n "Vamos começar a instalação!" 
-for i in $(seq 1 1 5);
+for i in $(seq 1 1 3);
 do
 	echo -n " ." 
 	sleep 01
@@ -10,8 +10,7 @@ done
 apt update && apt upgrade -y
 apt install apache2 php -y
 clear
-echo -n "Siga a (1 - ETAPA) do arquivo etapas.txt" 
-for i in $(seq 1 1 5);
+for i in $(seq 1 1 3);
 do
 	echo -n " ." 
 	sleep 01
@@ -19,28 +18,11 @@ do
 done 
 clear 
 #!/bin/bash
-function exibir_barra_carregamento() {
-    local progresso=0
-    local tempo_espera=1
-    local total_etapas=$1
-
-    while [[ $progresso -lt $total_etapas ]]; do
-        echo -n " ."
-        sleep $tempo_espera
-        progresso=$((progresso + 1))
-    done
-
-    echo ""
-}
-# Solicitar ao usuário o valor do "yourdomain"
 read -p "Digite o valor de 'yourdomain': " yourdomain
 clear
-echo "Vamos começar a instalação!"
-exibir_barra_carregamento 5
 apt update && apt upgrade -y
 apt install apache2 php -y
 clear
-exibir_barra_carregamento 5
 CONF_CONTENT="<VirtualHost *:80>
 ServerAdmin admin@moodle.yourdomain.com
 DocumentRoot /var/www/html/moodle/
@@ -65,22 +47,13 @@ if pgrep -x "nano" >/dev/null; then
 fi
 echo "Arquivo de configuração criado com sucesso em /etc/apache2/sites-available/moodle.yourdomain.com.conf"
 clear
-apt-get install mlocate
+apt-get install mlocate -y
 updatedb
 a2enmod rewrite
 sudo a2enmod rewrite
 systemctl restart apache2
 sudo a2ensite moodle.yourdomain.com
 systemctl reload apache2
-clear
-echo -n "Siga a (2 - ETAPA) do arquivo etapas.txt" 
-for i in $(seq 1 1 8);
-do
-	echo -n " ." 
-	sleep 01
-	echo -ne "" 
-done 
-clear 
 exibir_barra_carregamento 8
 NEW_VALUE="7000"
 PHP_INI_FILE="/etc/php/7.4/apache2/php.ini"
@@ -89,15 +62,6 @@ echo "O valor de 'max_input_vars' foi alterado para $NEW_VALUE no arquivo php.in
 systemctl restart apache2
 apt install mariadb-server -y
 clear
-echo -n "Siga a (3 - ETAPA) do arquivo etapas.txt" 
-for i in $(seq 1 1 8);
-do # Faça
-	echo -n " ." 
-	sleep 01
-	echo -ne "" 
-done 
-clear
-exibir_barra_carregamento 8
 mysql <<EOF
 CREATE DATABASE moodle_db DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER 'moodle_user'@'localhost' IDENTIFIED BY 'm0d1fyth15';
@@ -107,22 +71,12 @@ FLUSH PRIVILEGES;
 EOF
 echo "Os comandos SQL foram executados com sucesso no banco de dados."
 apt install git graphviz aspell ghostscript php7.4-{pspell,curl,gd,intl,mysql,xml,xmlrpc,ldap,zip,soap,mbstring} -y
-clear
 sudo apt install git -y
 clear
 cd /opt
 git clone https://github.com/moodle/moodle
 clear
-echo -n "Fazendo download o moodle via git" 
-for i in $(seq 1 1 8);
-do # Faça
-	echo -n " ." 
-	sleep 01
-	echo -ne "" 
-done 
-clear
 echo "Fazendo download do Moodle via Git"
-exibir_barra_carregamento 8
 cd moodle/
 git branch --track MOODLE_400_STABLE origin/MOODLE_400_STABLE
 git checkout MOODLE_400_STABLE
@@ -130,15 +84,6 @@ cp -R /opt/moodle/ /var/www/html/moodle/
 mkdir /var/www/moodledata
 chmod -R 777 /var/www/moodledata/
 chown -R www-data. /var/www/moodledata/ /var/www/html/moodle/
-clear
-echo "Vou te mostrar o IP e voce copia e cole na URL" 
-echo "Importante colocar '/moodle' no final." 
-for i in $(seq 1 1 5);
-do # Faça
-	echo -n " ." 
-	sleep 01
-	echo -ne "" 
-done 
 clear
 ip=$(hostname -I | awk '{print $1}')
 echo "Acesse o Moodle pelo seguinte link: https://$ip/moodle"
