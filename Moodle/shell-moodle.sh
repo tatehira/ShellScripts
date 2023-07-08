56e1326f09970a332ca61d2b8c8f494230f70206
tatehira <niickcostta00@outlook.com>
2023-07-07 22:12:14 -0400

    v2

--- Moodle/shell-moodle.sh
+++ Moodle/shell-moodle.sh
@@ -1,35 +1,57 @@
-#!/bin/bash
+!#/bin/bash/
+echo -n "Vamos começar a instalação!" 
+for i in $(seq 1 1 3);
+do
+	echo -n " ." 
+	sleep 01
+	echo -ne "" 
+done 
+read -p "Enter the domain name for Moodle (e.g., moodle.yourdomain.com): " domain
+apt update && apt upgrade -y
+apt install apache2 git php -y
+cat > /etc/apache2/sites-available/$domain.conf <<EOF
+<VirtualHost *:80>
+ServerAdmin admin@$domain
+DocumentRoot /var/www/html/moodle/
+ServerName $domain
 
-# Verifica se o script está sendo executado como root
-if [[ $EUID -ne 0 ]]; then
-   echo "Este script deve ser executado como root." 
-   exit 1
-fi
+<Directory /var/www/html/moodle/>
+Options +FollowSymlinks
+AllowOverride All
+Require all granted
+</Directory>
 
-# Atualiza o sistema
-apt update
-apt upgrade -y
+ErrorLog \${APACHE_LOG_DIR}/moodle.error.log
+CustomLog \${APACHE_LOG_DIR}/moodle.access.log combined
 
-# Instala os pacotes necessários
-apt install -y apache2 mariadb-server php php-mysql libapache2-mod-php unzip
-
-# Configura o MariaDB
-mysql_secure_installation
-
-# Cria o banco de dados e o usuário para o Moodle
-mysql -u root -p -e "CREATE DATABASE moodle DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-mysql -u root -p -e "CREATE USER 'moodleuser'@'localhost' IDENTIFIED BY 'password';"
-mysql -u root -p -e "GRANT ALL PRIVILEGES ON moodle.* TO 'moodleuser'@'localhost';"
-mysql -u root -p -e "FLUSH PRIVILEGES;"
-
-# Baixa e descompacta o Moodle
-cd /var/www/html
-git clone https://github.com/moodle/moodle.git
-mv moodle/* .
-chown -R www-data:www-data /var/www/html/
-chmod -R 755 /var/www/html/
-
-# Reinicia o Apache
+</VirtualHost>
+EOF
+apt-get install mlocate
+updatedb
+locate a2enmod
+sudo a2enmod rewrite
+sudo sudo a2ensite $domain.conf
+systemctl reload apache2
+sed -i 's/^max_input_vars = .*/max_input_vars = 7000/' /etc/php/7.4/apache2/php.ini
 systemctl restart apache2
-
-echo "A instalação do Moodle foi concluída. Acesse o site para continuar com a configuração."
+apt install mariadb-server -y
+mysql <<EOF
+CREATE DATABASE moodle_db DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER 'moodle_user'@'localhost' IDENTIFIED BY 'm0d1fyth15';
+GRANT ALL ON moodle_db.* TO 'moodle_user'@'localhost';
+FLUSH PRIVILEGES;
+\q
+EOF
+apt install git graphviz aspell ghostscript php7.4-{pspell,curl,gd,intl,mysql,xml,xmlrpc,ldap,zip,soap,mbstring} -y
+cd /opt
+git clone https://github.com/moodle/moodle.git
+cd moodle/
+git branch --track MOODLE_400_STABLE origin/MOODLE_400_STABLE
+git checkout MOODLE_400_STABLE
+cp -R /opt/moodle/ /var/www/html/moodle/
+mkdir /var/www/moodledata
+chmod -R 777 /var/www/moodledata/
+chown -R www-data. /var/www/moodledata/ /var/www/html/moodle/
+clear
+ip=$(hostname -I | awk '{print $1}')
+echo "Acesse o Moodle pelo seguinte link: https://$ip/moodle"
