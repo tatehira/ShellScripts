cc68cfc39f9f77c4c2a26138d0b12dd87fe6abeb
Nick Tatehira <68212041+tatehira@users.noreply.github.com>
2023-07-11 12:28:36 -0400

    Update gitlab-ce-setup.sh

--- Gitlab-CE/gitlab-ce-setup.sh
+++ Gitlab-CE/gitlab-ce-setup.sh
@@ -1,4 +1,5 @@
 !# /bin/bash
+clear
 sudo apt update && sudo apt full-upgrade -y
 sudo apt install curl vim openssh-server ca-certificates -y
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
