8a808f1cdfaebfff4e6e0a6f7fc6f6bc1af7b754
tatehira <niickcostta00@outlook.com>
2023-07-12 01:05:49 -0400

    Update moodle2

--- Moodle/moodle-setup-2.sh
+++ Moodle/moodle-setup-2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo apt update
-sudo apt upgrade -y
-sudo apt install apache2 mysql-client mysql-server php7.4 libapache2-mod-php7.4
+sudo apt update && sudo apt upgrade -y
+sudo apt install apache2 mysql-client mysql-server php7.4 libapache2-mod-php7.4 -y
 sudo apt install sudo php7.4-cli php7.4-gd php7.4-curl php7.4-intl php7.4-mbstring php7.4-xml php7.4-zip graphviz aspell ghostscript clamav php7.4-pspell php7.4-mysql php7.4-xmlrpc php7.4-ldap  php7.4-soap -y
 sudo mysql_secure_installation
 sudo mysql -u root -p -e "CREATE DATABASE moodle;"
