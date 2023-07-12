c298124db3ec18e80e93ed23353e91cef8be04ca
tatehira <niickcostta00@outlook.com>
2023-07-12 00:59:13 -0400

    Update setup2

--- Moodle/moodle-setup-2.sh
+++ Moodle/moodle-setup-2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade -y
-sudo apt install apache2 mariadb-server sudo apt-get install php7.4 php7.4-cli php7.4-gd php7.4-curl php7.4-intl php7.4-mbstring php7.4-xml php7.4-zip graphviz aspell ghostscript clamav php7.4-pspell php7.4-mysql php7.4-xmlrpc php7.4-ldap  php7.4-soap -y
+sudo apt install apache2 mysql-client mysql-server php7.4 libapache2-mod-php7.4
+sudo apt install sudo php7.4-cli php7.4-gd php7.4-curl php7.4-intl php7.4-mbstring php7.4-xml php7.4-zip graphviz aspell ghostscript clamav php7.4-pspell php7.4-mysql php7.4-xmlrpc php7.4-ldap  php7.4-soap -y
 sudo mysql_secure_installation
 sudo mysql -u root -p -e "CREATE DATABASE moodle;"
 sudo mysql -u root -p -e "GRANT ALL PRIVILEGES ON moodle.* TO 'moodleuser'@'localhost' IDENTIFIED BY '12345678';"
