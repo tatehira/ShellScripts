6a91a473992ce1000587af82baec0a47149cbead
Wendrew nick costa tatehira <niickcostta00@outlook.com>
2023-07-12 10:03:17 -0400

    Adicionado gitlab-ce

--- Gitlab-CE/gitlab-ce-setup.sh
+++ Gitlab-CE/gitlab-ce-setup.sh
@@ -1,11 +1,11 @@
 !# /bin/bash
 sudo apt update && sudo apt full-upgrade -y
-[ -f /var/run/reboot-required ] && sudo reboot -f
-sudo apt install curl vim openssh-server ca-certificates -y
-curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+sudo apt install curl openssh-server ca-certificates tzdata perl -y
+curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 ip=$(hostname -I | awk '{print $1}')
 export GITLAB_URL="http://$ip"
+sudo EXTERNAL_URL="${GITLAB_URL}" apt install gitlab-ce
 sudo gitlab-ctl reconfigure
-clear
 ip=$(hostname -I | awk '{print $1}')
-echo "Acesse o Gitlab-CE pelo seguinte link: https://$ip"
+clear
+echo "Acesse o Gitlab-CE pelo seguinte link: http://$ip"
