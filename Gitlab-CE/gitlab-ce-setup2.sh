ed47b687f68da79c0d908eea75b8df57b9f0db0f
Wendrew nick costa tatehira <niickcostta00@outlook.com>
2023-07-12 11:40:30 -0400

    update setup2

--- Gitlab-CE/gitlab-ce-setup2.sh
+++ Gitlab-CE/gitlab-ce-setup2.sh
@@ -1,4 +1,4 @@
-!# /bin/bash
+#!/bin/bash
 clear
 sudo systemctl stop apache2
 sudo apt update && sudo apt full-upgrade -y
@@ -9,9 +9,20 @@ export GITLAB_URL="http://$ip"
 sudo EXTERNAL_URL="${GITLAB_URL}" apt install gitlab-ce -y
 sudo gitlab-ctl reconfigure
 clear
+
+file_path="/etc/gitlab/initial_root_password"
+
+if [ -f "$file_path" ]; then
+    echo "Arquivo initial_root_password encontrado em $file_path"
+    password=$(grep "Password" "$file_path" | awk '{print $2}')
+    echo "Senha padrão encontrada no arquivo: $password"
+else
+    echo "Arquivo initial_root_password não encontrado em $file_path"
+fi
+clear
 ip=$(hostname -I | awk '{print $1}')
 echo "==============================================================="
 echo "-- Acesse o Gitlab-CE pelo seguinte link: https://$ip          "
 echo "==============================================================="
 echo "login: root"
-echo "senha: "
+echo "senha: $password"
