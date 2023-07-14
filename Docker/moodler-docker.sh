eec70d521167ac693cfc148d238eeb7fc34b537a
Tatehira <niickcostta00@outlook.com>
2023-07-14 15:51:50 -0400

    update docker moodle

--- Docker/moodler-docker.sh
+++ Docker/moodler-docker.sh
@@ -3,17 +3,13 @@ cd moodle-docker
 nano Dockerfile
 
 FROM php:7.4-apache
-# Instalar as extensões PHP necessárias
 RUN docker-php-ext-install mysqli pdo pdo_mysql
-# Configurar o ambiente do Apache
 RUN a2enmod rewrite
-# Copiar o código do Moodle para o diretório web do Apache
 COPY moodle /var/www/html/
-# Definir permissões adequadas
-RUN chown -R www-data:www-data /var/www/html/
-# Expor a porta do Apache
+RUN chmod 777 /var/www 
+RUN chown -R www-data:www-data /var/www/html/moodle
+RUN chmod -R 755 /var/www/html/moodle 
 EXPOSE 80
-# Comando de inicialização do Apache
 CMD ["apache2-foreground"]
 
 docker build -t moodle-image .
